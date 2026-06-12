00178abb9d1caee2590a624155a36b62f76e2a49
Kubik <84036232+KodeurKubik@users.noreply.github.com>
2026-06-12 17:30:07 +0200

    fix: new newsletter URL

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -5,7 +5,7 @@ export const SITE_ICON = "https://kursal.chat/icon.png";
 
 export const PAPER_URL = "/kursal.pdf";
 
-export const LISTMONK_URL = "https://newsletter.openvoxel.studio";
+export const LISTMONK_URL = "https://mail.kursal.chat";
 export const LISTMONK_LIST_UUID = "51120ca8-96d6-424f-aef0-011cf505d351";
 
 export const DOWNLOAD_PREFIX = "https://app.kursal.chat";
